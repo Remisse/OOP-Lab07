78797da101d38a57715c4c6d2a7cab2e88253f05
Remisse <zeromuseg7@gmail.com>
2020-11-10 17:44:41 +0100

    Complete exercise 3

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -14,4 +14,17 @@ public enum Sport {
      * declare the following sports: - basket - soccer - tennis - bike - F1 -
      * motogp - volley
      */
+	BASKET("Basket"),
+	BIKE("Bike"),
+	F1("Formula 1"),
+	MOTOGP("MotoGP"),
+	SOCCER("Soccer"),
+	TENNIS("Tennis"),
+	VOLLEY("Volleyball");
+	
+	private final String name;
+	
+	private Sport(final String name) {
+		this.name = name;
+	}
 }
