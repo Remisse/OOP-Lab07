8f2fbc5a0bbd5fda0a766c86fb30ec4ec1025003
Remisse <zeromuseg7@gmail.com>
2020-11-11 20:40:59 +0100

    Remove unnecessary 'if' statement from addSport method

--- src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
@@ -105,9 +105,7 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      *            a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-    	if (!this.hasSport(sport)) {
-    		this.sports.add(sport);
-    	}
+    	this.sports.add(sport);
     }
 
     /**
