668db9ca1b54bf6d3a4a210286d96d7ed3ca394e
Remisse <zeromuseg7@gmail.com>
2020-11-11 20:17:14 +0100

    Add 'this' keyword to fields referenced in hashCode() and equals()

--- src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
@@ -145,7 +145,7 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
         public int hashCode() {
 			final int prime = 31;
 			int result = 1;
-			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
 			return result;
 		}
 
@@ -154,13 +154,13 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
 				return true;
 			if (obj == null)
 				return false;
-			if (getClass() != obj.getClass())
+			if (this.getClass() != obj.getClass())
 				return false;
 			Sport other = (Sport) obj;
-			if (name == null) {
+			if (this.name == null) {
 				if (other.name != null)
 					return false;
-			} else if (!name.equals(other.name))
+			} else if (!this.name.equals(other.name))
 				return false;
 			return true;
 		}
